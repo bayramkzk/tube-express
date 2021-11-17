0ac67674b19d8013dd67a3363f1d0cdb988b5b26
bayramdev <48856944+bayramkazik@users.noreply.github.com>
2021-11-17 22:12:18 +0300

    feat: create router.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const path = require("path");
+const { router } = require("./router");
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
@@ -10,9 +11,7 @@ app.set("view engine", "ejs");
 const public = express.static(path.join(__dirname, "public"));
 app.use("/public", public);
 
-app.get("/", (req, res) => {
-  res.render("home", { name: req.query.name || "user" });
-});
+app.use(router);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
