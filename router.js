a17c410433c05362eebddb119497b8509f7488e3
bayramdev <48856944+bayramkazik@users.noreply.github.com>
2021-11-17 23:19:11 +0300

    feat: add appName to every views title

--- router.js
+++ router.js
@@ -1,24 +1,25 @@
 const express = require("express");
+const config = require("./config");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", config);
 });
 
 router.get("/stars", (req, res) => {
-  return res.render("stars");
+  return res.render("stars", config);
 });
 
 router.get("/tags", (req, res) => {
-  return res.render("tags");
+  return res.render("tags", config);
 });
 
 router.get("/favorites", (req, res) => {
-  return res.render("favorites");
+  return res.render("favorites", config);
 });
 
 router.get("/history", (req, res) => {
-  return res.render("history");
+  return res.render("history", config);
 });
 
 module.exports = { router };
