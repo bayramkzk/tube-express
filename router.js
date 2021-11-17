cb8d40fe3896611a7c224efe7311ef6486b551f3
bayramdev <48856944+bayramkazik@users.noreply.github.com>
2021-11-17 23:08:47 +0300

    refactor: rename home.ejs to index.ejs

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("home", { name: req.query.name || "user" });
+  res.render("index");
 });
 
 router.get("/stars", (req, res) => {
