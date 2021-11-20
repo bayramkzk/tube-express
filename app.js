c16855bd09cdb4b941657ac28e90e7ed54c7c52e
bayramdev <48856944+bayramkazik@users.noreply.github.com>
2021-11-20 23:17:44 +0300

    feat: set up mongoose

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const router = require("./routes");
 
-const app = express();
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use("/public", express.static(path.join(__dirname, "public")));
-app.use(router);
+const main = async () => {
+  const app = express();
+  app.set("views", path.join(__dirname, "views"));
+  app.set("view engine", "ejs");
+  app.use("/public", express.static(path.join(__dirname, "public")));
+  app.use(router);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+  await mongoose.connect(process.env.DATABASE_URL);
+  console.log("Connected to MongoDB database")
+
+  const PORT = process.env.PORT;
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+};
+
+main();
