fda5a22e719420b7ee316644e3a5493644abe2c5
bayramdev <48856944+bayramdev@users.noreply.github.com>
2021-11-28 16:30:40 +0300

    feat: integrate video-grid to home route

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const Video = require("../models/video");
 const config = require("../config");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  const opts = {
+router.get("/", async (req, res) => {
+  const recentVideos = await Video.find()
+    .sort({ createdAt: "descending" })
+    .limit(config.maxVideoCountPerPage);
+
+  const videoCount = await Video.find().count();
+  const pageCount = Math.ceil(videoCount / config.maxVideoCountPerPage);
+
+  res.render("home", {
     appName: config.appName,
-  };
-  res.render("home", opts);
+    videos: recentVideos,
+    pageCount,
+    pageIndex: 0,
+    headerText: "Home",
+  });
 });
 
 module.exports = router;
